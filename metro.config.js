a2feea4bb7b8aa9161e5116685a0e1a53aa701f8
Leonardo Oliveira <leoazaz40@gmail.com>
2026-05-18 18:53:33 -0300

    Fix: Movendo wasm para as extensoes de assets

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-if (!config.resolver.sourceExts.includes('wasm')) {
-  config.resolver.sourceExts.push('wasm');
+if (!config.resolver.assetExts.includes('wasm')) {
+  config.resolver.assetExts.push('wasm');
 }
-
 module.exports = config;
