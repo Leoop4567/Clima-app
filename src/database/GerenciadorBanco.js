dad6828e49e6605224a15001ac141187afd4c423
Leonardo Oliveira <leoazaz40@gmail.com>
2026-05-18 19:04:59 -0300

    Fix: Adicionando suporte a localStorage para evitar tela branca na Web

--- src/database/GerenciadorBanco.js
+++ src/database/GerenciadorBanco.js
@@ -1,9 +1,14 @@
+import { Platform } from 'react-native'; 
 import * as SQLite from 'expo-sqlite';
 
-const banco = SQLite.openDatabaseSync('clima.db');
+
+const banco = Platform.OS !== 'web' ? SQLite.openDatabaseSync('clima.db') : null;
 
 export const GerenciadorBanco = {
   configurarBanco: async () => {
+
+    if (Platform.OS === 'web') return;
+    
     await banco.execAsync(`
       CREATE TABLE IF NOT EXISTS historico_busca (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,12 +18,24 @@ export const GerenciadorBanco = {
   },
 
   salvarCidade: async (nome) => {
-    try {
 
+    if (Platform.OS === 'web') {
+      try {
+        let historico = JSON.parse(localStorage.getItem('historico_busca') || '[]');
+        historico = historico.filter(cidade => cidade !== nome); 
+        historico.unshift(nome); 
+        historico = historico.slice(0, 4); 
+        localStorage.setItem('historico_busca', JSON.stringify(historico));
+      } catch (e) {
+        console.error("Erro no localStorage da Web:", e);
+      }
+      return;
+    }
+
+l
+    try {
       await banco.runAsync('DELETE FROM historico_busca WHERE nome_cidade = ?', [nome]);
       await banco.runAsync('INSERT INTO historico_busca (nome_cidade) VALUES (?)', [nome]);
-      
-
       await banco.runAsync(`
         DELETE FROM historico_busca WHERE id NOT IN (
           SELECT id FROM historico_busca ORDER BY id DESC LIMIT 4
@@ -31,6 +48,15 @@ export const GerenciadorBanco = {
 
   buscarHistorico: async () => {
 
+    if (Platform.OS === 'web') {
+      try {
+        return JSON.parse(localStorage.getItem('historico_busca') || '[]');
+      } catch (e) {
+        return [];
+      }
+    }
+
+ 
     const resultados = await banco.getAllAsync('SELECT nome_cidade FROM historico_busca ORDER BY id DESC');
     return resultados.map(row => row.nome_cidade);
   }
